ac438e1f6ba2daec5565bf736d554da0d0f47757
Boris Durisic <bokac279@gmail.com>
2019-09-03 16:26:08 +0100

    fixed union creation

--- app/controllers/strikes_controller.rb
+++ app/controllers/strikes_controller.rb
@@ -13,6 +13,8 @@ class StrikesController < ApplicationController
   def create
     @strike = Strike.new(strike_params)
     authorize @strike
+
+    
     if @strike.save
       redirect_to root_path
     else
@@ -48,12 +50,23 @@ class StrikesController < ApplicationController
   private
 
   def strike_params
-    params.require(:strike).permit(:sector_id,
-                                   :category_id,
-                                   :organization,
-                                   :description,
-                                   :start_date,
-                                   :end_date,
-                                   union_attributes: [:id, :name, :initials, :url])
+    # raise
+    unless (params.require(:strike).has_key?(:union_attributes))
+      params.require(:strike).permit(:sector_id,
+                                    :category_id,
+                                    :organization,
+                                    :description,
+                                    :start_date,
+                                    :end_date,
+                                    :union_id)
+    else
+      params.require(:strike).permit(:sector_id,
+                                    :category_id,
+                                    :organization,
+                                    :description,
+                                    :start_date,
+                                    :end_date,
+                                    union_attributes: [:id, :name, :initials, :url])
+    end
   end
 end
