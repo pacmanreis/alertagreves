3951bafc99cc8edeec98c93b05ef6d444cde191b
Paulo Reis <pacmanreis@gmail.com>
2019-09-03 15:10:57 +0100

    Implemented pundit

---
+++ app/policies/strike_policy.rb
@@ -0,0 +1,23 @@
+class StrikePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end
