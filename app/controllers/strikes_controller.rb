6eff73c6abfd78b88ee3de9565525b9e133c7be6
Paulo Reis <pacmanreis@gmail.com>
2019-09-03 12:41:47 +0100

    Updated new view, and implemented create action

--- app/controllers/strikes_controller.rb
+++ app/controllers/strikes_controller.rb
@@ -1,5 +1,4 @@
 class StrikesController < ApplicationController
-
   def index
     @strikes = Strike.all
   end
@@ -8,7 +7,28 @@ class StrikesController < ApplicationController
     @strike = Strike.new
   end
 
+  def create
+    @strike = Strike.new(strike_params)
+    if @strike.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
     @strike = Strike.find(params[:id])
+  end
+
+  private
+
+  def strike_params
+    params.require(:strike).permit(:sector_id,
+                                   :category_id,
+                                   :union_id,
+                                   :organization,
+                                   :description,
+                                   :start_date,
+                                   :end_date)
   end
 end
