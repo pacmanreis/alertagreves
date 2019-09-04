da8e27360e5bcf43d86d6a686b02cad3cae317e9
Paulo Reis <pacmanreis@gmail.com>
2019-09-05 00:14:51 +0100

    Implemented i18n for multilanguage and improved navbar design

--- config/application.rb
+++ config/application.rb
@@ -22,3 +22,11 @@ module Alertagreves
     # the framework and any gems in your application.
   end
 end
+
+class Application < Rails::Application
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
+  config.i18n.available_locales = [:en, :pt]
+  config.i18n.default_locale = :pt
+end
