c0ea3eb6c5cbe9bd77d6b9c6fb6a8c58e2792966
Boris Durisic <bokac279@gmail.com>
2019-09-11 13:49:19 +0100

    fixed mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://alertagreves.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://alertagreves.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
